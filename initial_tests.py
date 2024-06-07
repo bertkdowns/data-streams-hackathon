b15e88404cde6ea3fdcf8714cebcc39e529a5b5e
bd65 <bd65@students.waikato.ac.nz>
2024-06-07 14:38:01 +1200

    use regression

--- initial_tests.py
+++ initial_tests.py
@@ -1,7 +1,8 @@
 from river import linear_model
-from river.metrics import Accuracy
+from river.metrics import MSE
 from river.evaluate import progressive_val_score
 from river import stream
+from river import forest
 
 model =   linear_model.LogisticRegression()#insert Classifier/algorithm here
 
@@ -14,14 +15,49 @@ params = {
     'parse_dates': {'year': '%Y'}
 }
 
+def parse_float(x):
+    try:
+        return float(x)
+    except ValueError:
+        return None
+
+
 # https://riverml.xyz/latest/api/stream/iter-csv/
-dataset = stream.iter_csv('../power_plant/MHPA-docs-data.csv', 
+dataset = stream.iter_csv('../power_plant/test_data.csv', 
                         target="SlrW_Avg",
                         converters={
-                            'Rain_mm':int
+                            'Rain_mm':int,
+                            'batt_volt': parse_float,
+                            'mean_wind_speed': parse_float,
+                            'mean_wind_direction': parse_float,
+                            'std_wind_dir': parse_float,
+                            'Max_Gust_Min': parse_float,
+                            'Max_Gust_Hr': parse_float,
+                            'Barametric_Avg': parse_float,
+                            'Air_Temp_Avg': parse_float,
+                            'RH_Avg': parse_float,
+                            'in_bytes_str': parse_float,
+                            'Dew_Point_Avg': parse_float,
+                            'SlrW_Avg': parse_float,
                         },
                         drop_nones= True,
                         drop=["TmStamp","RecNum","SlrMJ_Tot"] )
 
-for x, y in dataset:
-    print(x, y)
+# for x, y in dataset:
+#     print(x, y)
+
+print("Loaded data")
+
+model = forest.ARFRegressor()
+
+score = progressive_val_score(dataset=dataset,
+                        model=model,
+                        metric=MSE(),
+                        show_memory=True,
+                        show_time=True,
+                        print_every=50000)  #gives us the current Metric every X datapoint
+
+print(score)
+
+
+
